c3f85636ef3213fe54c200585a7dd56079d88078
Aman Kumar Gupta <agupta11040@gmail.com>
2026-01-04 23:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (92.90%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,30 @@
+class Solution {
+    private double[][] memo;
+
+    public double soupServings(int n) {
+        // Optimization: probability converges to 1
+        if (n >= 4800) return 1.0;
+
+        int m = (n + 24) / 25; // scale down
+        memo = new double[m + 1][m + 1];
+
+        return dfs(m, m);
+    }
+
+    private double dfs(int a, int b) {
+        if (a <= 0 && b <= 0) return 0.5;
+        if (a <= 0) return 1.0;
+        if (b <= 0) return 0.0;
+
+        if (memo[a][b] > 0) return memo[a][b];
+
+        memo[a][b] = 0.25 * (
+                dfs(a - 4, b) +
+                dfs(a - 3, b - 1) +
+                dfs(a - 2, b - 2) +
+                dfs(a - 1, b - 3)
+        );
+
+        return memo[a][b];
+    }
+}
