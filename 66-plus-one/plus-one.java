c817ea29eed2b0ce460ba73b2073185e19ceff59
Aman Kumar Gupta <agupta11040@gmail.com>
2026-02-15 10:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (37.73%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] plusOne(int[] digits) {
 
-        for (int i = digits.length - 1; i >= 0; i--) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
 
             if (digits[i] < 9) {
                 digits[i]++;
@@ -11,8 +13,9 @@ class Solution {
             digits[i] = 0;
         }
 
-        int[] result = new int[digits.length + 1];
+        int[] result = new int[n + 1];
         result[0] = 1;
+
         return result;
     }
 }
