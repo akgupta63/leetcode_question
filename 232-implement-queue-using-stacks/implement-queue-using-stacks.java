f81827fcb20de3834cd51e530280c7861c7ab0ed
Aman Kumar Gupta <agupta11040@gmail.com>
2026-02-15 18:03:00 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.22%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    private void shiftStacks() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+
+    public int pop() {
+        shiftStacks();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        shiftStacks();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
