0d52f72998a18722751b35fcfea5bdcf2f9f240e
Aman Kumar Gupta <agupta11040@gmail.com>
2026-02-17 19:31:32 +0530

    Time: 1 ms (88.29%) | Memory: 43.5 MB (12.69%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        int capitals = 0;
+
+        for (char c : word.toCharArray()) {
+            if (Character.isUpperCase(c)) capitals++;
+        }
+
+        if (capitals == n) return true;
+        if (capitals == 0) return true;
+        if (capitals == 1 && Character.isUpperCase(word.charAt(0))) return true;
+
+        return false;
+    }
+}
