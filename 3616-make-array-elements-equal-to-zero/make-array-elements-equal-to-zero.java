862f328fc37c6d2b2cedaee76ff78b879d9ac663
Aman Kumar Gupta <agupta11040@gmail.com>
2026-03-26 12:55:41 +0530

    Time: 102 ms (15.47%) | Memory: 45.1 MB (14.21%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,27 +1,36 @@
 class Solution {
 
     public int countValidSelections(int[] nums) {
+        int count = 0;
+        int nonZeros = 0;
         int n = nums.length;
-        int ans = 0;
-        int sum = 0;
-        for (int x : nums) {
-            sum += x;
-        }
-        int leftSum = 0;
-        int rightSum = sum;
+        for (int x : nums) if (x > 0) nonZeros++;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
-                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
-                    ans++;
-                }
-                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
-                    ans++;
-                }
-            } else {
-                leftSum += nums[i];
-                rightSum -= nums[i];
+                if (isValid(nums, nonZeros, i, -1)) count++;
+                if (isValid(nums, nonZeros, i, 1)) count++;
             }
         }
-        return ans;
+        return count;
+    }
+
+    private boolean isValid(
+        int[] nums,
+        int nonZeros,
+        int start,
+        int direction
+    ) {
+        int n = nums.length;
+        int curr = start;
+        int[] temp = nums.clone();
+        while (nonZeros > 0 && curr >= 0 && curr < n) {
+            if (temp[curr] > 0) {
+                temp[curr]--;
+                direction *= -1;
+                if (temp[curr] == 0) nonZeros--;
+            }
+            curr += direction;
+        }
+        return nonZeros == 0;
     }
 }
