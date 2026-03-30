97b8dcee2f1396fa33bb96138f3fbf75a735cd84
Aman Kumar Gupta <agupta11040@gmail.com>
2026-03-30 23:04:39 +0530

    Time: 21 ms (86.63%) | Memory: 178.2 MB (49.10%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -5,13 +5,13 @@ class Solution {
         for (int num : nums) {
             valuesToRemove.add(num);
         }
+
         while (head != null && valuesToRemove.contains(head.val)) {
             head = head.next;
         }
         if (head == null) {
             return null;
         }
-
         ListNode current = head;
         while (current.next != null) {
             if (valuesToRemove.contains(current.next.val)) {
