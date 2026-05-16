3b61bb7e8e84932a1333c53164f5951e2ea968ac
Aman Kumar Gupta <agupta11040@gmail.com>
2026-05-16 21:12:07 +0530

    Time: 198 ms (35.11%) | Memory: 240 MB (84.47%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.java
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.java
@@ -2,80 +2,63 @@ import java.util.*;
 
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-
         Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
 
-        boolean[] hasSecret = new boolean[n];
-        hasSecret[0] = true;
-        hasSecret[firstPerson] = true;
+        boolean[] know = new boolean[n];
+        know[0] = true;
+        know[firstPerson] = true;
 
         int i = 0;
+
         while (i < meetings.length) {
             int time = meetings[i][2];
 
-            Map<Integer, Integer> parent = new HashMap<>();
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+            Set<Integer> people = new HashSet<>();
 
-            // Find with path compression
-            java.util.function.IntUnaryOperator find = new java.util.function.IntUnaryOperator() {
-                @Override
-                public int applyAsInt(int x) {
-                    if (parent.get(x) != x)
-                        parent.put(x, applyAsInt(parent.get(x)));
-                    return parent.get(x);
-                }
-            };
-
-            // Union
-            java.util.function.BiConsumer<Integer, Integer> union = (a, b) -> {
-                int pa = find.applyAsInt(a);
-                int pb = find.applyAsInt(b);
-                if (pa != pb)
-                    parent.put(pb, pa);
-            };
-
-            List<Integer> people = new ArrayList<>();
-
-            // Collect all meetings at same time
             while (i < meetings.length && meetings[i][2] == time) {
                 int x = meetings[i][0];
                 int y = meetings[i][1];
 
-                parent.putIfAbsent(x, x);
-                parent.putIfAbsent(y, y);
+                graph.computeIfAbsent(x, k -> new ArrayList<>()).add(y);
+                graph.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
 
-                union.accept(x, y);
                 people.add(x);
                 people.add(y);
+
                 i++;
             }
 
-            // Group by root
-            Map<Integer, List<Integer>> groups = new HashMap<>();
-            for (int p : people) {
-                int root = find.applyAsInt(p);
-                groups.computeIfAbsent(root, k -> new ArrayList<>()).add(p);
+            Queue<Integer> queue = new LinkedList<>();
+            Set<Integer> visited = new HashSet<>();
+
+            for (int person : people) {
+                if (know[person]) {
+                    queue.offer(person);
+                    visited.add(person);
+                }
             }
 
-            // Spread secret
-            for (List<Integer> group : groups.values()) {
-                boolean spread = false;
-                for (int p : group) {
-                    if (hasSecret[p]) {
-                        spread = true;
-                        break;
+            while (!queue.isEmpty()) {
+                int curr = queue.poll();
+                know[curr] = true;
+
+                for (int next : graph.getOrDefault(curr, new ArrayList<>())) {
+                    if (!visited.contains(next)) {
+                        visited.add(next);
+                        queue.offer(next);
                     }
-                }
-                if (spread) {
-                    for (int p : group)
-                        hasSecret[p] = true;
                 }
             }
         }
 
         List<Integer> result = new ArrayList<>();
-        for (int p = 0; p < n; p++)
-            if (hasSecret[p])
-                result.add(p);
+
+        for (int j = 0; j < n; j++) {
+            if (know[j]) {
+                result.add(j);
+            }
+        }
 
         return result;
     }
